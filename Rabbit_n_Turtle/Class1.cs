48d19868938284d78591e728d02b32a47114aac0
The4yo <eugshpak@yandex.ru>
2023-09-21 20:02:28 +0300

    Добавьте файлы проекта.

---
+++ Rabbit_n_Turtle/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Rabbit_n_Turtle
+{
+    internal class AnimalThread
+    {
+        public string name;
+        public Thread thread;
+        public AnimalThread(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ Rabbit_n_Turtle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace Rabbit_n_Turtle
+{
+    class Program
+    {
+        public static int x = 0;
+        public static int y = 0;
+        static void Main(string[] args)
+        {
+            Thread rabbit = new Thread(RabbitRun);
+            Thread turtle = new Thread(TurtleRun);
+            rabbit.Start();
+            turtle.Start();
+
+            while (x == 50 || y == 50)
+            {
+                if (x > (y + 5))
+                {
+                    turtle.Priority = ThreadPriority.Normal;
+                    rabbit.Priority = ThreadPriority.Lowest;
+                }
+                if (y > (x + 5))
+                {
+                    turtle.Priority = ThreadPriority.Lowest;
+                    rabbit.Priority = ThreadPriority.Normal;
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+                public static void RabbitRun()
+                {
+                    for (x = 0; x < 200; x++)
+                    {
+                        Console.WriteLine($"Кролик пробежал " + x + " метров");
+                    }
+                }
+
+                public static void TurtleRun()
+                {
+                    for (x = 0; x < 200; x++)
+                    {
+                        Console.WriteLine($"Черепаха пробежала " + x + " метров");
+                    }
+                }
+            
+        
+    }
+}
